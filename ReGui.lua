f9b07285c4dff3653bb69161751551160155706f
fewfwegwe774 <gregwgefwfwq@gmail.com>
2026-04-05 21:46:16 +0200

    Update ReGui.lua

--- ReGui.lua
+++ ReGui.lua
@@ -16,7 +16,7 @@ local ReGui = {
 	Version = "1.3.9",
 	Author = "Depso",
 	License = "MIT",
-	Repository = "https://github.com/depthso/Dear-ReGui/",
+	Repository = "https://github.com/fewfwegwe774/b/",
 	
 	--// Configuration
 	Debug = false,
